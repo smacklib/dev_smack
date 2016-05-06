64bad118f0a4bd148b938925baddd6b3dacb6f62
Michael Binz <michab66@gmail.com>
2016-05-06 08:17:28 +0000

    Reworked Service management.

---
+++ src/java/org/jdesktop/util/ServiceManager.java
@@ -0,0 +1,95 @@
+/* $Id$
+ *
+ * Smack Application.
+ *
+ * Released under Gnu Public License
+ * Copyright © 2015 Michael G. Binz
+ */
+package org.jdesktop.util;
+
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jdesktop.smack.util.ReflectionUtils;
+
+/**
+ * Management of ApplicationServices.
+ *
+ * @version $Rev$
+ * @author Michael Binz
+ */
+public final class ServiceManager
+{
+    /**
+     * The map of singular application services.
+     */
+    private static final Map<Class<?>, Object> _singletons =
+            new HashMap<>();
+
+    /**
+     * Create an instance.
+     */
+    public ServiceManager()
+    {
+        // Catch ctor.
+    }
+
+    /**
+     * Get an application service of the specified type.
+     *
+     * @param singletonType The type of the application service.
+     * @return An instance of the requested service.
+     */
+    public static synchronized <T> T getApplicationService( Class<T> singletonType )
+    {
+        if ( !  _singletons.containsKey( singletonType ) )
+            try
+            {
+                _singletons.put(
+                        singletonType,
+                        singletonType.newInstance() );
+            }
+            catch ( Exception e )
+            {
+                throw new RuntimeException( e );
+            }
+
+        return singletonType.cast( _singletons.get( singletonType ) );
+    }
+
+    /**
+     * Get an application service of the specified type.
+     *
+     * @param singletonType The type of the application service.
+     * @return An instance of the requested service.
+     */
+    public static synchronized <T> T initApplicationService(
+            Class<T> singletonType,
+            Object...objects  )
+    {
+        if ( _singletons.containsKey(  singletonType ) )
+            throw new IllegalArgumentException( "Already initialized: " + singletonType.getName() );
+
+        @SuppressWarnings("unchecked")
+        Constructor<T> c = ReflectionUtils.matchConstructorArguments(
+                (Constructor<T>[])singletonType.getConstructors(),
+                objects );
+
+        try
+        {
+            if ( ! c.isAccessible() )
+                c.setAccessible( true );
+
+            T result = c.newInstance( objects );
+
+            _singletons.put( singletonType, result );
+
+            return result;
+        }
+        catch ( Exception e )
+        {
+            throw new RuntimeException( e );
+        }
+    }
+}
