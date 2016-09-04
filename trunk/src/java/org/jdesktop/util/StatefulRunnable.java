46cc99aa2e5b28aeb5969fefd0e17f651b14c140
michab66 <michab66@901972f4-fc98-46ed-8fba-68aad6a9eff3>
2016-09-04 13:32:03 +0000

    Completed javadocs.

--- trunk/src/java/org/jdesktop/util/StatefulRunnable.java
+++ trunk/src/java/org/jdesktop/util/StatefulRunnable.java
@@ -1,10 +1,15 @@
+/* $Id: Loader.java 142 2015-04-14 18:46:49Z michab66 $
+ *
+ * Released under Gnu Public License
+ * Copyright © 2008 Michael G. Binz
+ */
 package org.jdesktop.util;
 
 /**
+ * A runnable that keeps a state object.
  *
+ * @version $Rev$
  * @author Michael
- *
- * @param <T>
  */
 public abstract class StatefulRunnable<T> implements Runnable
 {
@@ -15,13 +20,25 @@ public abstract class StatefulRunnable<T> implements Runnable
         _state = state;
     }
 
+    /**
+     * Access the state.
+     */
     protected T getState()
     {
         return _state;
     }
 
+    /**
+     * Entered if the Runnable is started.
+     *
+     * @param state The state set in the constructor.
+     * @see Runnable#run()
+     */
     abstract void run( T state );
 
+    /**
+     * Calls {@link #run(Object)}.
+     */
     @Override
     public void run()
     {
