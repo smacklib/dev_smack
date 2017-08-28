3b6a1a78a67d6d87a5f28667b53b138c884ec8e6
michab66 <michab66@901972f4-fc98-46ed-8fba-68aad6a9eff3>
2017-08-28 20:07:11 +0000

    Made WeakMapWithProducer public.

--- trunk/src/java/org/jdesktop/util/WeakMapWithProducer.java
+++ trunk/src/java/org/jdesktop/util/WeakMapWithProducer.java
@@ -1,15 +1,20 @@
+/* $Id$
+ *
+ * Released under Gnu Public License
+ * Copyright © 2017 Michael G. Binz
+ */
 package org.jdesktop.util;
 
 import java.util.WeakHashMap;
 import java.util.function.Function;
 
 /**
- *
+ * A weak map that produces content on demand using a factory.
  *
  * @version $Revision$
  * @author Michael Binz
  */
-class WeakMapWithProducer<K,V>
+public class WeakMapWithProducer<K,V>
 {
     private final WeakHashMap<K, V> _cache =
             new WeakHashMap<>();
