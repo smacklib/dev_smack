831aa609208b8057b600c0420b716a3ebd4f28b8
michab66 <michab66@901972f4-fc98-46ed-8fba-68aad6a9eff3>
2018-01-28 11:22:07 +0000

    StringConverter does not longer use unquoting.

--- trunk/src/java/org/jdesktop/util/converters/StringStringConverter.java
+++ trunk/src/java/org/jdesktop/util/converters/StringStringConverter.java
@@ -9,15 +9,23 @@ package org.jdesktop.util.converters;
 
 import org.jdesktop.util.ResourceConverter;
 import org.jdesktop.util.ResourceMap;
-import org.jdesktop.util.StringUtil;
 
 /**
+ * The trivial converter.
  *
  * @version $Rev$
  * @author Michael Binz
  */
-public class StringStringConverter extends ResourceConverter {
+public class StringStringConverter extends ResourceConverter
+{
+    // Remark: Do not perform unquoting here.  This was once the
+    // case and made the converter pretty inconvenient to use
+    // in cases where quotes are actually needed. (Remember that
+    // backslash in resource files is line continuation and
+    // cannot be used for escape purposes.)
 
+    // Think if this converter is needed at all or if better the
+    // conversion engine should detect that no conversion is needed.
     public StringStringConverter() {
         super(String.class);
     }
@@ -25,6 +33,6 @@ public class StringStringConverter extends ResourceConverter {
     @Override
     public Object parseString( String s, ResourceMap notUsed )
     {
-        return StringUtil.unquote( s );
+        return s;
     }
 }
