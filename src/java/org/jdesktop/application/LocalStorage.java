e43b731222937d8753cb2e8221f6e745d82280d8
Michael Binz <michab66@gmail.com>
2018-01-28 11:42:18 +0000

    Dead wood.

--- src/java/org/jdesktop/application/LocalStorage.java
+++ src/java/org/jdesktop/application/LocalStorage.java
@@ -32,7 +32,6 @@ import javax.jnlp.FileContents;
 import javax.jnlp.PersistenceService;
 import javax.jnlp.ServiceManager;
 
-import org.jdesktop.application.util.AppHelper;
 import org.jdesktop.application.util.PlatformType;
 import org.jdesktop.beans.AbstractBeanEdt;
 import org.jdesktop.util.StringUtil;
@@ -330,7 +329,7 @@ public final class LocalStorage extends AbstractBeanEdt {
             } catch (SecurityException ignore) {
             }
             if (userHome != null) {
-                final PlatformType osId = AppHelper.getPlatform();
+                final PlatformType osId = PlatformType.getPlatform();
                 if (osId == PlatformType.WINDOWS) {
                     File appDataDir = null;
                     try {
