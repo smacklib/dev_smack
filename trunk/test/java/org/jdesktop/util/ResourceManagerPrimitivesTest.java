67f45a1aa1e9d1499aa30f12ca11a27d0f0f71ae
michab66 <michab66@901972f4-fc98-46ed-8fba-68aad6a9eff3>
2018-03-07 19:52:54 +0000

    Dead wood.

--- trunk/test/java/org/jdesktop/util/ResourceManagerPrimitivesTest.java
+++ trunk/test/java/org/jdesktop/util/ResourceManagerPrimitivesTest.java
@@ -3,8 +3,7 @@ package org.jdesktop.util;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-import javax.annotation.Resource;
-
+import org.jdesktop.util.ResourceManager.Resource;
 import org.junit.Before;
 import org.junit.Test;
 
