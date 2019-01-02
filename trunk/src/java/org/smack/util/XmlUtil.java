d92db7f4e64738c945480859871d2632650575fd
michab66 <michab66@901972f4-fc98-46ed-8fba-68aad6a9eff3>
2019-01-02 10:15:52 +0000

    Added Xml utilities.

---
+++ trunk/src/java/org/smack/util/XmlUtil.java
@@ -0,0 +1,54 @@
+/**
+ * $Id$
+ *
+ * Unpublished work.
+ * Copyright © 2018 Michael G. Binz
+ */
+package org.smack.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.Reader;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class XmlUtil
+{
+
+    /**
+     * Transform a file based on an XSLT transformation.
+     *
+     * @param xslt The transformation.
+     * @param toTransform The file to transform.
+     * @return The result of the transformation.
+     * @throws Exception In case of an error.
+     */
+    public static String transform( InputStream xslt, Reader toTransform )
+            throws Exception
+    {
+        TransformerFactory  tFactory =
+                TransformerFactory.newInstance();
+
+        Source xslSource = new
+            StreamSource( xslt );
+        Transformer transformer =
+            tFactory.newTransformer( xslSource );
+
+        ByteArrayOutputStream result =
+                new ByteArrayOutputStream();
+        transformer.transform(
+                new StreamSource( toTransform ),
+                new StreamResult( result ) );
+
+        return result.toString();
+    }
+
+    private XmlUtil()
+    {
+        throw new AssertionError();
+    }
+}
