28eb9ebb080c7b0f2a69722b5d5f6b8703650ceb
michab66 <michab66@901972f4-fc98-46ed-8fba-68aad6a9eff3>
2019-02-26 19:58:59 +0000

    EnumArray added.

---
+++ trunk/src/java/org/smack/util/EnumArray.java
@@ -0,0 +1,83 @@
+/* $Id: EnumArray.java 1755 2017-10-03 10:57:39Z Michael $
+ *
+ * Unpublished work.
+ * Copyright © 2016 Michael G. Binz
+ */
+package org.smack.util;
+
+import java.util.Arrays;
+
+/**
+ * An array indexed by an enum type.
+ *
+ * <V> The array's content type.
+ * <K> The enum type.
+ *
+ * @version $Rev: 1755 $
+ * @author Michael Binz
+ */
+public class EnumArray<K extends Enum<K>, V>
+{
+    private final V _null;
+    private final Object[] _values;
+
+    /**
+     *
+     * @param c
+     * @param nullValue
+     */
+    public EnumArray( Class<K> c, V nullValue )
+    {
+        _values =
+                new Object[ c.getEnumConstants().length ];
+        _null = nullValue;
+
+        clear();
+    }
+
+    /**
+     * Set a value at an index.
+     * @param idx The index.
+     * @param value The value.
+     */
+    public void set( K idx, V value )
+    {
+        _values[ idx.ordinal() ] = value;
+    }
+
+    /**
+     * Get the value at index.  If this was not set, then the
+     * null value is returned.
+     * @param idx
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public V get( K idx )
+    {
+        return (V)_values[ idx.ordinal() ];
+    }
+
+    /**
+     * @return The array's size.
+     */
+    public int size()
+    {
+        return _values.length;
+    }
+
+    /**
+     * Clear the array's content.
+     */
+    public void clear()
+    {
+        Arrays.fill( _values, _null );
+    }
+
+    /**
+     * @return The array's null value as passed into the constructor.
+     */
+    public V getNull()
+    {
+        return _null;
+    }
+}
