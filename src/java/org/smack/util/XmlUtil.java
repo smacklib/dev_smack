29fd82779cea94685d867a171a42dbfbd2aa28bd
Michael Binz <michael.binz@daimler.com>
2019-06-24 16:57:11 +0200

    Added transform( File, File ).

--- src/java/org/smack/util/XmlUtil.java
+++ src/java/org/smack/util/XmlUtil.java
@@ -7,6 +7,8 @@
 package org.smack.util;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
@@ -64,6 +66,37 @@ public class XmlUtil
         return transform(
                 new StreamSource( xslt ),
                 new InputSource( toTransform ) );
+    }
+
+    /**
+     * Transform a file based on an XSLT transformation. Access to
+     * non-existent dtds is ignored.  This version of the operation
+     * internally sets the systemId of the xslt, so that access on the
+     * stylesheet via the xls 'document( '' )' operation works.
+     *
+     * @param xslt The transformation.
+     * @param toTransform The file to transform.
+     * @return The result of the transformation.
+     * @throws Exception In case of an error.
+     */
+    public static String transform( File xslt, File toTransform )
+            throws Exception
+    {
+        try ( Reader xsltReader = new FileReader( xslt ) )
+        {
+            try ( Reader toTransformReader = new FileReader( toTransform ) )
+            {
+                StreamSource xsltSource =
+                        new StreamSource( xsltReader );
+                xsltSource.setSystemId(
+                        xslt );
+
+                return transform(
+                        xsltSource,
+                        new InputSource( toTransformReader ) );
+            }
+
+        }
     }
 
     /**
