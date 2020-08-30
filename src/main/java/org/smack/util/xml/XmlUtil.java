ed4d702d53bbf47456da47aeef2d137161bb5af0
Michael Binz <michab66@gmail.com>
2020-08-30 15:06:08 +0200

    Added xpath processing. Reworked tests.

--- src/main/java/org/smack/util/xml/XmlUtil.java
+++ src/main/java/org/smack/util/xml/XmlUtil.java
@@ -8,13 +8,15 @@ package org.smack.util.xml;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -25,8 +27,13 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -39,6 +46,24 @@ import org.xml.sax.XMLReader;
  */
 public class XmlUtil
 {
+    /**
+     * A resolver that ignores access to non-existent dtds.
+     */
+    private static EntityResolver  EMPTY_DTD_RESOLVER = new EntityResolver()
+    {
+        @Override
+        public InputSource resolveEntity(
+                String publicId,
+                String systemId)
+                        throws SAXException, IOException
+        {
+            if (systemId.endsWith(".dtd"))
+                return new InputSource(new StringReader(" "));
+
+            return null;
+        }
+    };
+
     /**
      * Transform a file based on an XSLT transformation. Access to
      * non-existent dtds is ignored.
@@ -87,20 +112,10 @@ public class XmlUtil
     public static String transform( File xslt, File toTransform )
             throws Exception
     {
-        try ( Reader xsltReader = new FileReader( xslt ) )
-        {
-            try ( Reader toTransformReader = new FileReader( toTransform ) )
-            {
-                StreamSource xsltSource =
-                        new StreamSource( xsltReader );
-                xsltSource.setSystemId(
-                        xslt );
-
-                return transform(
-                        xsltSource,
-                        new InputSource( toTransformReader ) );
-            }
-        }
+        return transform(
+                xslt,
+                toTransform,
+                Collections.emptyMap() );
     }
 
     /**
@@ -119,22 +134,8 @@ public class XmlUtil
                 SAXParserFactory.newInstance().newSAXParser().
                 getXMLReader();
 
-        // Set a resolver that ignores access to non-existent dtds.
-        reader.setEntityResolver(new EntityResolver()
-        {
-            @Override
-            public InputSource resolveEntity(
-                    String publicId,
-                    String systemId)
-                            throws SAXException, IOException
-            {
-                if (systemId.endsWith(".dtd"))
-                    return new InputSource(new StringReader(" "));
-
-                return null;
-            }
-        });
-
+        reader.setEntityResolver(
+                EMPTY_DTD_RESOLVER );
         TransformerFactory tFactory =
                 TransformerFactory.newInstance();
         Transformer transformer =
@@ -151,15 +152,18 @@ public class XmlUtil
     }
 
     /**
-     * This operation works on Java 8.  TODO(micbinz) integrate.
+     * Transform a file based on an XSLT transformation. Access to
+     * non-existent dtds is ignored.  This version of the operation
+     * internally sets the systemId of the xslt, so that access on the
+     * stylesheet via the xls 'document( '' )' operation works.
      *
-     * @param stylesheet The stylesheet.
-     * @param datafile The input to process.
-     * @param parameters Parameters to be passed to the stylesheet.
-     * @return The processing result.
+     * @param stylesheet The transformation.
+     * @param datafile The file to transform.
+     * @param parameters Parameters for the stylesheet.
+     * @return The result of the transformation.
      * @throws Exception In case of an error.
      */
-    public static String transform8(
+    public static String transform(
             File stylesheet,
             File datafile,
             Map<String,Object> parameters )
@@ -172,22 +176,8 @@ public class XmlUtil
         DocumentBuilder builder =
                 factory.newDocumentBuilder();
 
-        // Set a resolver that ignores access to non-existent dtds.
-        builder.setEntityResolver(new EntityResolver()
-        {
-            @Override
-            public InputSource resolveEntity(
-                    String publicId,
-                    String systemId)
-                            throws SAXException, IOException
-            {
-                if (systemId.endsWith(".dtd"))
-                    return new InputSource(new StringReader(" "));
-
-                return null;
-            }
-        });
-
+        builder.setEntityResolver(
+                EMPTY_DTD_RESOLVER );
         Document document =
                 builder.parse(datafile);
         TransformerFactory tFactory =
@@ -235,22 +225,8 @@ public class XmlUtil
         DocumentBuilder builder =
                 factory.newDocumentBuilder();
 
-        // Set a resolver that ignores access to non-existent dtds.
-        builder.setEntityResolver(new EntityResolver()
-        {
-            @Override
-            public InputSource resolveEntity(
-                    String publicId,
-                    String systemId)
-                            throws SAXException, IOException
-            {
-                if (systemId.endsWith(".dtd"))
-                    return new InputSource(new StringReader(" "));
-
-                return null;
-            }
-        });
-
+        builder.setEntityResolver(
+                EMPTY_DTD_RESOLVER );
         Document document =
                 builder.parse(datafile);
         TransformerFactory tFactory =
@@ -276,26 +252,125 @@ public class XmlUtil
         return bos.toString();
     }
 
-    public static String transform8(
-            InputStream stylesheet,
-            InputStream datafile )
-            throws Exception
+    /**
+     * Evaluate an xpath against an XML-document.
+     *
+     * @param xmlDocument The document.
+     * @param expression The xpath.
+     * @return The result of the expression.  The empty string if the
+     * expression did not select data.
+     * @throws Exception In case of an error.  Note that it is no error
+     * if the xpath does not select data.
+     */
+    public static String getXPath(
+            File xmlDocument,
+            String expression )
+                    throws Exception
     {
-        return transform8(
-                stylesheet,
-                datafile,
-                Collections.emptyMap() );
+        return getXPath(
+                xmlDocument,
+                new String[] { expression } )
+                    .get( 0 );
     }
 
-    public static String transform8(
-            File stylesheet,
-            File datafile )
-            throws Exception
+    public static <R> R getXPathAs(
+            File xmlDocument,
+            String xpath,
+            Function<String, R> converter ) throws Exception
     {
-        return transform8(
-                stylesheet,
-                datafile,
-                Collections.emptyMap() );
+        return converter.apply(
+                getXPath( xmlDocument, xpath ) );
+    }
+
+    public static double getXPathAsDouble(
+            File xmlDocument,
+            String expression
+            ) throws Exception
+    {
+        return getXPathAs(
+                xmlDocument,
+                expression,
+                Double::parseDouble );
+    }
+
+    /**
+     * Evaluate a set of xpath-expressions against an XML-document.
+     *
+     * @param xmlDocument The document.
+     * @param expressions The xpath.
+     * @return The result of the expressions in a list.
+     * @throws Exception In case of an error.
+     * @see #getXPath(File, String)
+     */
+    public static List<String> getXPath(
+            File xmlDocument,
+            String ... expressions
+            ) throws Exception
+    {
+        DocumentBuilderFactory factory =
+                DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware( true );
+
+        DocumentBuilder builder =
+                factory.newDocumentBuilder();
+        org.w3c.dom.Document doc =
+                builder.parse( xmlDocument );
+        XPathFactory xPathfactory =
+                XPathFactory.newInstance();
+        XPath xpath =
+                xPathfactory.newXPath();
+
+        xpath.setNamespaceContext(
+                getNamespaces( xmlDocument ) );
+
+        var result =
+                new ArrayList<String>();
+
+        for ( String c : expressions )
+        {
+            XPathExpression expr =
+                    xpath.compile( c );
+
+            result.add(
+                    expr.evaluate( doc, XPathConstants.STRING ).toString() );
+        }
+
+        return result;
+    }
+
+    private static NamespaceContextImpl getNamespaces( File xmlDocument ) throws Exception
+    {
+        DocumentBuilderFactory factory =
+                DocumentBuilderFactory.newInstance();
+        // Needs to be true for getLocalName() below to return meaningful data.
+        factory.setNamespaceAware( true );
+
+        DocumentBuilder builder =
+                factory.newDocumentBuilder();
+        org.w3c.dom.Document doc =
+                builder.parse( xmlDocument );
+        XPathFactory xPathfactory =
+                XPathFactory.newInstance();
+        XPath xpath =
+                xPathfactory.newXPath();
+
+        XPathExpression expr = xpath.compile( "//namespace::*" );
+
+        NodeList x = (NodeList)expr.evaluate( doc, XPathConstants.NODESET );
+
+        NamespaceContextImpl result =
+                new NamespaceContextImpl();
+
+        for ( int i = 0 ; i < x.getLength() ; i++ )
+        {
+            var q =
+                    x.item( i );
+            result.put(
+                    q.getLocalName(),
+                    q.getNodeValue() );
+        }
+
+        return result;
     }
 
     private XmlUtil()
