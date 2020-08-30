e0786a7bb938c73e805fe2ad44a148c4f0f3c81b
Michael Binz <michab66@gmail.com>
2020-08-30 19:38:18 +0200

    Using new xmlutils.

--- src/test/java/org/smack/fx/SvgTest.java
+++ src/test/java/org/smack/fx/SvgTest.java
@@ -1,8 +1,11 @@
 package org.smack.fx;
 
-import java.io.File;
+import java.io.InputStream;
 
+import org.smack.util.Disposer;
+import org.smack.util.StringUtil;
 import org.smack.util.xml.XmlUtil;
+import org.smack.util.xml.XmlUtilTest;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -19,65 +22,80 @@ import javafx.stage.Stage;
  */
 public class SvgTest extends Application{
 
-	private final int MIN_BUTTON_SIZE = 10;
+    private final int MIN_BUTTON_SIZE = 10;
+
+//    private static final String SVG_NAME = "ic_car.xml";
+    private static final String SVG_NAME = "ic_menu_help.xml";
+
+    private InputStream getRegisteredStream( Disposer d, String name )
+    {
+        return d.register( XmlUtilTest.class.getResourceAsStream( name ) );
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        SVGPath svg = new SVGPath();
+        try ( Disposer d = new Disposer() )
         {
-        var content =
-                XmlUtil.getXPath(
-                        new File( "ic_car.xml" ), "vector/path/@android:pathData" );
-        svg.setContent( content );
-        }
-        {
-        var tint =
-                XmlUtil.getXPathAs(
-                        Color::web,
-                        new File( "ic_car.xml" ),
-                        "vector/@android:tint" );
-        svg.setFill( tint );
-        }
 
-        var viewportHeight =
-                XmlUtil.getXPathAs(
-                        Double::parseDouble,
-                        new File( "ic_car.xml" ), "vector/@android:viewportHeight" );
-        var viewportWidth =
-                XmlUtil.getXPathAs(
-                        Double::parseDouble,
-                        new File( "ic_car.xml" ), "vector/@android:viewportWidth" );
+            SVGPath svg = new SVGPath();
+            {
+                var content =
+                        XmlUtil.getXPath(
+                                getRegisteredStream( d, SVG_NAME ),
+                                "vector/path/@android:pathData" );
+                svg.setContent( content );
+            }
+            {
+                var tint =
+                        XmlUtil.getXPath(
+                                getRegisteredStream( d, SVG_NAME ),
+                                "vector/@android:tint" );
+                if ( StringUtil.hasContent( tint ) )
+                    svg.setFill( Color.web( tint ) );
+            }
 
-        Button buttonWithGraphics = new Button();
-        buttonWithGraphics.setGraphic(svg);
+            var viewportHeight =
+                    XmlUtil.getXPathAs(
+                            Double::parseDouble,
+                            getRegisteredStream( d, SVG_NAME ),
+                            "vector/@android:viewportHeight" );
+            var viewportWidth =
+                    XmlUtil.getXPathAs(
+                            Double::parseDouble,
+                            getRegisteredStream( d, SVG_NAME ),
+                            "vector/@android:viewportWidth" );
 
-        // Bind the Image scale property to the buttons size
-        svg.scaleXProperty().bind(
-                buttonWithGraphics.widthProperty().divide( viewportWidth ));
-        svg.scaleYProperty().bind(
-                buttonWithGraphics.heightProperty().divide( viewportHeight ));
+            Button buttonWithGraphics = new Button();
+            buttonWithGraphics.setGraphic(svg);
 
-        // Declare a minimum size for the button
-        buttonWithGraphics.setMinSize(
-                MIN_BUTTON_SIZE,
-                MIN_BUTTON_SIZE);
+            // Bind the Image scale property to the buttons size
+            svg.scaleXProperty().bind(
+                    buttonWithGraphics.widthProperty().divide( viewportWidth ));
+            svg.scaleYProperty().bind(
+                    buttonWithGraphics.heightProperty().divide( viewportHeight ));
 
-        HBox root = new HBox();
-        root.setAlignment(Pos.CENTER);
-        root.getChildren().addAll(buttonWithGraphics);
-        root.layoutBoundsProperty().addListener((observableValue, oldBounds, newBounds) -> {
+            // Declare a minimum size for the button
+            buttonWithGraphics.setMinSize(
+                    MIN_BUTTON_SIZE,
+                    MIN_BUTTON_SIZE);
+
+            HBox root = new HBox();
+            root.setAlignment(Pos.CENTER);
+            root.getChildren().addAll(buttonWithGraphics);
+            root.layoutBoundsProperty().addListener((observableValue, oldBounds, newBounds) -> {
                 double size = Math.max(MIN_BUTTON_SIZE, Math.min(newBounds.getWidth(), newBounds.getHeight()));
                 buttonWithGraphics.setPrefSize(size, size);
             }
-        );
+                    );
 
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
     }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
